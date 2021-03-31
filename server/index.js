cb6c5ae3e90f06126aff7cb801617948bcfadba5
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-03-31 16:56:16 -0300

    Adds routes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,24 @@
 import express from "express"
 import bodyParser from "body-parser"
+import dotenv from "dotenv"
 import mongoose from "mongoose"
 import cors from "cors"
 
+import postRoutes from "./routes/posts.js"
+
+const app = express()
+dotenv.config()
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server runnig on port: ${ PORT }`)))
+    .catch((error) => console.log(error))
+
+mongoose.set('useFindAndModify', false)
