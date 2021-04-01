c52325a0dcdd9940b69e82ae2cd8ae1a8820dcf9
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-04-01 12:47:05 -0300

    Adds delete post feature

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -3,11 +3,14 @@ import { Card, CardActions, CardContent, CardMedia, Button, Typography  } from '
 import ThumbUpIcon from '@material-ui/icons/ThumbUp'
 import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import { useDispatch } from 'react-redux'
 
 import moment from 'moment'
 import useStyles from './styles'
+import { deletePost } from '../../../actions/posts'
 
 const Post = ({ post, setCurrentId }) => {
+    const dispatch = useDispatch()
     const classes = useStyles()
 
     return (
@@ -35,7 +38,7 @@ const Post = ({ post, setCurrentId }) => {
                     Like
                     { post.likeCount }
                 </Button>
-                <Button size="small" color="primary" onClick={() => {}}>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small" />
                     Delete
                 </Button>
