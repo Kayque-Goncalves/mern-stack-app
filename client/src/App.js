40bcf0b831035b6565019156d4820d22583a3315
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-04-01 12:24:58 -0300

    Adds update posts feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
-import { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 
 import { getPosts } from './actions/posts'
@@ -8,12 +8,13 @@ import Posts from './components/Posts/Posts'
 import useStyles from './styles'
 
 function App() {
+    const [ currentId, setCurrentId ] = useState(null)
     const classes = useStyles()
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(getPosts())
-    }, [dispatch])
+    }, [setCurrentId, dispatch])
 
     return (
         <Container maxwidth="lg">
@@ -24,10 +25,10 @@ function App() {
                 <Container>
                     <Grid container justify="space-between" alignItems="stretch" spacing={ 3 }>
                         <Grid item xs={ 12 } sm={ 7 }>
-                            <Posts />
+                            <Posts setCurrentId={ setCurrentId }/>
                         </Grid>
                         <Grid item xs={ 12 } sm={ 4 }>
-                            <Form />
+                            <Form currentId={ currentId } setCurrentId={ setCurrentId }/>
                         </Grid>
                     </Grid>
                 </Container>
