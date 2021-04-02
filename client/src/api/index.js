64d185d634a824a82ee96b7464b422b6705f3718
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-04-02 10:52:44 -0300

    Updates index.js file to setup netlify setup

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+// const url = 'https://mern-stack-study.herokuapp.com/posts'
 const url = 'http://localhost:5000/posts'
 
 export const fetchPosts = () => axios.get(url)
