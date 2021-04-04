9534b729acfb60be08c9d40b83d7ddd4acd59def
Kayque Gonçalves <kayque.gsilva229@gmail.com>
2021-04-04 11:26:15 -0300

    Bug fix: Unauthenticated error when like a post

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,6 +3,8 @@ import jwt from 'jsonwebtoken'
 
 import User from '../models/user.js'
 
+const secret = 'test'
+
 export const signin = async (req, res) => {
     const { email, password } = req.body
     try {
@@ -12,7 +14,7 @@ export const signin = async (req, res) => {
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password)
         if(!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials." })
 
-        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: '1h' })
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, secret, { expiresIn: '1h' })
         res.status(200).json({ result: existingUser, token })
     } catch (error) {
         res.status(500).json({ message: 'Something went wrong.' })
@@ -29,7 +31,7 @@ export const signup = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 12)
         const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` })
-        const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: '1h' })
+        const token = jwt.sign({ email: result.email, id: result._id }, secret, { expiresIn: '1h' })
         
         res.status(200).json({ result, token })
     } catch (error) {
